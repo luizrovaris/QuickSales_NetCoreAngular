237e6d18efba33505d05133887570bb7d7628854
Luiz Rovaris <luiz_rovaris@hotmail.com>
2020-10-20 19:36:33 +0100

    QuickSales - Update login rule

--- NetCoreAngular/QuickSales/QuickSales.Web/ClientApp/src/app/user/login/login.component.ts
+++ NetCoreAngular/QuickSales/QuickSales.Web/ClientApp/src/app/user/login/login.component.ts
@@ -1,24 +1,28 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { User } from "../../model/user";
-import { Router } from "@angular/router";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"]
 })
-export class LoginComponent {
-
+export class LoginComponent implements OnInit {
   public user;
+  public returnUrl: string;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private activatedRouter: ActivatedRoute) {
+  }
+  ngOnInit(): void {
+    this.returnUrl = this.activatedRouter.snapshot.queryParams['returnUrl'];
     this.user = new User();
   }
 
   login(): void {
     if (this.user.email == 'lr@mail.com' && this.user.password == '1234') {
       sessionStorage.setItem('authenticated-user', '1');
-      this.router.navigate(['/']);
+
+      this.router.navigate([(this.returnUrl ? this.returnUrl : '/')]);
     }
   }
 }
