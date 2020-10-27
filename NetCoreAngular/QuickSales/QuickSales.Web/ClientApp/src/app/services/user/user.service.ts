7ff2aa09fdffc0538641fc953dc2b59e84275991
Luiz Rovaris <luiz_rovaris@hotmail.com>
2020-10-27 18:28:26 +0000

    QuickSales - Creating UserService structure

--- NetCoreAngular/QuickSales/QuickSales.Web/ClientApp/src/app/services/user/user.service.ts
+++ NetCoreAngular/QuickSales/QuickSales.Web/ClientApp/src/app/services/user/user.service.ts
@@ -1,7 +1,25 @@
 import { Injectable, Inject } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { User } from "../../model/user";
 
+@Injectable({
+  providedIn: "root"
+})
 export class UserService {
 
+  private baseUrl: string;
+  constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    this.baseUrl = baseUrl;
+  }
+
+  public checkUser(user: User): Observable<User> {
+    const header = new HttpHeaders().set('content-type', 'application/json');
+    var body = {
+      email: user.email,
+      password: user.password
+    };
+
+    return this.http.post<User>(this.baseUrl + "api/user", body, { headers: header });
+  }
 }
