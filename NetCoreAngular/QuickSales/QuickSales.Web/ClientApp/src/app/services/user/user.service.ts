9c0d77b2428792379aaa241c8f0ec76edf3b5bed
Luiz Rovaris <luiz_rovaris@hotmail.com>
2020-10-27 18:10:48 +0000

    QuickSales - Creating UserService class

--- NetCoreAngular/QuickSales/QuickSales.Web/ClientApp/src/app/services/user/user.service.ts
+++ NetCoreAngular/QuickSales/QuickSales.Web/ClientApp/src/app/services/user/user.service.ts
@@ -1,1 +1,7 @@
-﻿
+import { Injectable, Inject } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+export class UserService {
+
+}
