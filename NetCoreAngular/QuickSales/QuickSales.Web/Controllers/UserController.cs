e02ad58926ce915c3196c60246801db54cd8abe8
Luiz Rovaris <luiz_rovaris@hotmail.com>
2020-10-28 18:51:47 +0000

    QuickSales - Adding usercontroller

---
+++ NetCoreAngular/QuickSales/QuickSales.Web/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuickSales.Domain.Entities;
+using System;
+
+namespace QuickSales.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost("login")]
+        public ActionResult Login([FromBody] User user)
+        {
+            ActionResult result;
+
+            try
+            {
+                if (user.Email == "lr@mail.com" && user.Password == "1234")
+                {
+                    result = Ok(user);
+                }
+                else
+                {
+                    result = BadRequest("Login invalid");
+                }
+            }
+            catch (Exception ex)
+            {
+                result = BadRequest(ex);
+            }
+
+            return result;
+        }
+    }
+}
