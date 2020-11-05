df7dac1134ab01278be5d6cb9149f4d32da18a9a
Luiz Rovaris <luiz_rovaris@hotmail.com>
2020-11-05 19:32:53 +0000

    QuickSales - Updating register user ts

--- NetCoreAngular/QuickSales/QuickSales.Web/ClientApp/src/app/user/register/register.user.component.ts
+++ NetCoreAngular/QuickSales/QuickSales.Web/ClientApp/src/app/user/register/register.user.component.ts
@@ -1,10 +1,19 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { User } from "../../model/user";
+import { UserService } from "../../services/user/user.service";
 
 @Component({
   selector: "register-user",
   templateUrl: "./register.user.component.html",
   styleUrls: ["./register.user.component.css"]
 })
-export class RegisterUserComponent {
+export class RegisterUserComponent implements OnInit {
+  public user: User;
 
+  constructor(private userService: UserService) {
+
+  }
+  ngOnInit(): void {
+    this.user = new User();
+    }
 }
