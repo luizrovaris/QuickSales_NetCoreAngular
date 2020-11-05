21b6a5b2cfd73816f0eecea3baef4917dfbc632f
Luiz Rovaris <luiz_rovaris@hotmail.com>
2020-11-05 19:14:21 +0000

    QuickSales - Updating register user files

--- NetCoreAngular/QuickSales/QuickSales.Web/ClientApp/src/app/app.module.ts
+++ NetCoreAngular/QuickSales/QuickSales.Web/ClientApp/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
 import { LoginComponent } from './user/login/login.component';
+import { RegisterUserComponent } from './user/register/register.user.component';
 import { RouteGuards } from './authorization/route.guards';
 import { UserService } from './services/user/user.service';
 
@@ -18,7 +19,8 @@ import { UserService } from './services/user/user.service';
     NavMenuComponent,
     HomeComponent,
     ProductComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterUserComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -27,7 +29,8 @@ import { UserService } from './services/user/user.service';
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'product', component: ProductComponent, canActivate: [RouteGuards]},
-      { path: 'login', component: LoginComponent}
+      { path: 'login', component: LoginComponent },
+      { path: 'register-user', component: RegisterUserComponent}
     ])
   ],
   providers: [UserService],
--- NetCoreAngular/QuickSales/QuickSales.Web/ClientApp/src/app/user/register/register.user.component.ts
+++ NetCoreAngular/QuickSales/QuickSales.Web/ClientApp/src/app/user/register/register.user.component.ts
@@ -1,1 +1,10 @@
-﻿
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "register-user",
+  templateUrl: "./register.user.component.html",
+  styleUrls: ["./register.user.component.css"]
+})
+export class RegisterUserComponent {
+
+}
