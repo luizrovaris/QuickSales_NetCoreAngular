7c00c6b013e52e4b694aa2eebbb84e3ae1543164
Luiz Rovaris <luiz_rovaris@hotmail.com>
2020-11-19 20:05:58 +0000

    QuickSales - Creating base entity method to get error message.

--- NetCoreAngular/QuickSales/QuickSales.Domain/Entities/Base/BaseEntity.cs
+++ NetCoreAngular/QuickSales/QuickSales.Domain/Entities/Base/BaseEntity.cs
@@ -9,6 +9,7 @@ namespace QuickSales.Domain.Entities.Base
         {
             this.validationMessages = new List<string>();
         }
+
         public int Id { get; set; }
 
         private List<string> validationMessages { get; set; }
@@ -17,12 +18,19 @@ namespace QuickSales.Domain.Entities.Base
         {
             this.validationMessages.Add(message);
         }
+
+        public string GetMessages()
+        {
+            return string.Join(". ", this.validationMessages);
+        }
+
         protected void ClearMessages()
         {
             this.validationMessages.Clear();
         }
 
         public abstract void Validate();
+
         public bool IsValid
         {
             get
