b8612fe7cd47b93932755af2951f942a97496bb7
Luiz Rovaris <luiz_rovaris@hotmail.com>
2020-11-19 19:24:22 +0000

    QuickSales - Creating product's file mapping.

---
+++ NetCoreAngular/QuickSales/QuickSales.Repository/Migrations/20201119185317_NewColunProductFile.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QuickSales.Repository.Migrations
+{
+    public partial class NewColunProductFile : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "File",
+                table: "Products",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "File",
+                table: "Products");
+        }
+    }
+}
