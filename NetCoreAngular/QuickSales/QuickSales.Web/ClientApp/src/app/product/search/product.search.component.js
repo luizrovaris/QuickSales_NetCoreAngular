79dc88cd7d7141c540d228debc31a6590aebdcd5
Luiz Rovaris <luiz_rovaris@hotmail.com>
2020-11-24 22:28:33 +0000

    QuickSales - Creating product search files on Angular.

---
+++ NetCoreAngular/QuickSales/QuickSales.Web/ClientApp/src/app/product/search/product.search.component.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ProductSearchComponent = /** @class */ (function () {
+    function ProductSearchComponent() {
+    }
+    return ProductSearchComponent;
+}());
+exports.ProductSearchComponent = ProductSearchComponent;
+//# sourceMappingURL=product.search.component.js.map
