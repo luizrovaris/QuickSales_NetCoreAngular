8a78b08baa5c7d46846161320c3fa1264b57b7bc
Luiz Rovaris <luiz_rovaris@hotmail.com>
2020-12-02 19:03:21 +0000

    QuickSales - Getting products to display on home page.

--- NetCoreAngular/QuickSales/QuickSales.Web/ClientApp/src/app/store/search/store.search.component.ts
+++ NetCoreAngular/QuickSales/QuickSales.Web/ClientApp/src/app/store/search/store.search.component.ts
@@ -11,7 +11,14 @@ export class StoreSearchComponent implements OnInit {
   public products: Product[];
 
   constructor(private productService: ProductService) {
-
+    this.productService.getAll().subscribe(
+      products => {
+        this.products = products;
+      },
+      e => {
+        console.log(e.error);
+      }
+    );
   }
   ngOnInit(): void {
   }
