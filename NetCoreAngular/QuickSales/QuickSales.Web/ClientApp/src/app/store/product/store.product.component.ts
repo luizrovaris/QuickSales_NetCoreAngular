5f88c2c9a59d6d001da84ea4e26e7197b15dd743
Luiz Rovaris <luiz_rovaris@hotmail.com>
2020-12-08 20:54:37 +0000

    QuickSales - Creating product detail template.

--- NetCoreAngular/QuickSales/QuickSales.Web/ClientApp/src/app/store/product/store.product.component.ts
+++ NetCoreAngular/QuickSales/QuickSales.Web/ClientApp/src/app/store/product/store.product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Product } from "../../model/product";
 
 @Component({
   selector: "store-app-product",
@@ -6,9 +7,16 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./store.product.component.css"]
 })
 export class StoreProductComponent implements OnInit {
+  public product: Product;
+
   constructor() {
 
   }
   ngOnInit(): void {
+    var productOnSession = sessionStorage.getItem('productDetailSession');
+    if (productOnSession) {
+      this.product = JSON.parse(productOnSession);
+      sessionStorage.setItem('productDetailSession', null);
+    }
   }
 }
