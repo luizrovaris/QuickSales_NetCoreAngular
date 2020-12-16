4c14381fa6e9f1356866dcf8b1f84a96830b1640
Luiz Rovaris <luiz_rovaris@hotmail.com>
2020-12-16 16:57:22 +0000

    QuickSales - Adding shopping cart on menu.

--- NetCoreAngular/QuickSales/QuickSales.Web/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ NetCoreAngular/QuickSales/QuickSales.Web/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../services/user/user.service';
+import { StoreBasket } from '../store/basket/store.basket';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.css']
 })
-export class NavMenuComponent {
+export class NavMenuComponent implements OnInit {
   isExpanded = false;
+  public basket: StoreBasket;
 
   constructor(private route: Router, private userService: UserService) {
+  }
+  ngOnInit(): void {
+    this.basket = new StoreBasket();
   }
 
   get user() {
@@ -36,5 +41,9 @@ export class NavMenuComponent {
   public logout() {
     this.userService.clearSession();
     this.route.navigate(['/']);
+  }
+
+  public isBasketEmpty(): boolean {
+    return this.basket.isBasketEmpty();
   }
 }
