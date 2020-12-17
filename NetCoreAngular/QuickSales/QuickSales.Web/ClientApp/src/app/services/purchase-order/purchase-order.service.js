01fabb8603a50e55bf4c3a95e37654425fc7b803
Luiz Rovaris <luiz_rovaris@hotmail.com>
2020-12-17 11:11:38 +0000

    QuickSales - Creating PurchaseOrderService.

---
+++ NetCoreAngular/QuickSales/QuickSales.Web/ClientApp/src/app/services/purchase-order/purchase-order.service.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var PurchaseOrderService = /** @class */ (function () {
+    function PurchaseOrderService() {
+    }
+    return PurchaseOrderService;
+}());
+exports.PurchaseOrderService = PurchaseOrderService;
+//# sourceMappingURL=purchase-order.service.js.map
---
+++ NetCoreAngular/QuickSales/QuickSales.Web/ClientApp/src/app/services/purchase-order/purchase-order.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, Inject } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { PurchaseOrder } from "../../model/purchaseOrder";
+
+@Injectable({
+  providedIn: "root"
+})
+export class PurchaseOrderService {
+  public baseUrl: string;
+
+  constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    this.baseUrl = baseUrl;
+  }
+
+  get header(): HttpHeaders {
+    return new HttpHeaders().set('content-type', 'application/json');
+  }
+
+  public savePurchaseOrder(purchaseOrder: PurchaseOrder): Observable<number> {
+    return this.http.post<number>(this.baseUrl + "api/purchaseorder", JSON.stringify(purchaseOrder), , { headers: this.header });
+  }
+}
